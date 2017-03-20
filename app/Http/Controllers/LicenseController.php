a1e473cba788fd347a1a9f35973b8a3223387fee
Ryan Eby <eby@ryaneby.com>
2017-03-20 14:49:24 -0400

    stub license model and controller

---
+++ app/Http/Controllers/LicenseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Couchcat\Http\Controllers;
+
+use Couchcat\License;
+use Illuminate\Http\Request;
+
+class LicenseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Couchcat\Licenses  $licenses
+     * @return \Illuminate\Http\Response
+     */
+    public function show(License $licenses)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Couchcat\Licenses  $licenses
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(License $licenses)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Couchcat\Licenses  $licenses
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, License $licenses)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Couchcat\Licenses  $licenses
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(License $licenses)
+    {
+        //
+    }
+}
---
+++ app/License.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Couchcat;
+
+use Illuminate\Database\Eloquent\Model;
+
+class License extends Model
+{
+    //
+}
