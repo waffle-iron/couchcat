db91d36becbcefbbeeb19e14a3ba0167955166bf
Ryan Eby <eby@ryaneby.com>
2017-03-21 09:35:42 -0400

    seeder for license database

--- database/seeds/LicenseTableSeeder.php
+++ database/seeds/LicenseTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class LicenseTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(LicenseTableSeeder::class);
+        factory(Couchcat\License::class, 20)->create();
     }
 }
