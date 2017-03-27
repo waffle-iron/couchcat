1dcb071cfb9f7029365add9364db35d07f2a9027
Ryan Eby <eby@ryaneby.com>
2017-03-27 17:05:26 -0400

    change name of license stub to reflect that it is for stats database

--- database/migrations/2017_03_08_150708_create_licenses_table.php
+++ database/migrations/2017_03_08_150708_create_licenses_table.php
@@ -16,7 +16,7 @@ class CreateLicensesTable extends Migration
         Schema::create('licenses', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('vendor_id');
-            $table->string('license-stub')->unique();
+            $table->string('statistics_stub')->unique();
             $table->date('starts');
             $table->date('expires')->nullable();
             $table->boolean('public')->default(0);
