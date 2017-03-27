317f9fd443fe3d64b9de4fd8c1b0226816bd475e
Ryan Eby <eby@ryaneby.com>
2017-03-27 16:11:44 -0400

    basic table and form for licenses

--- resources/views/license/index.blade.php
+++ resources/views/license/index.blade.php
@@ -3,11 +3,31 @@
 @section('title', 'Licenses')
 
 @section('content')
-<ul>
-@foreach($licenses as $license)
-
-<li>{{ $license }}</li>
-
+<table>
+	<thead>
+		<tr>
+		<th class='td-3'>Vendor</th>
+		<th class='td-2'>Catalog Licensee</th>
+		<th class='td-1'>Starts</th>
+		<th class='td-1'>Expires</th>
+		<th class='td-1'>Public</th>
+		<th class='td-1'>Works</th>
+		<th class='td-2'>Actions</th>
+		</tr>
+	</thead>
+	<tbody>
+	@foreach($licenses as $license)
+		<tr>
+			<td class='td-3'>{{ $license['vendor'] }}</td>
+			<td class='td-2'>{{ $license['license-stub'] }}</td>
+			<td class='td-1'>{{ $license['starts'] }}</td>
+			<td class='td-1'>{{ $license['expires'] }}</td>
+			<td class='td-1'>{{ $license['public'] }}</td>
+			<td class='td-1'>-</td>
+			<td class='td-2'><a href="{{ route('license.edit',['id' => $license['id']]) }}">Edit</a></td>
+		</tr>
 @endforeach
-</ul>
+	</tbody>
+</table>
+<a href='{{ route('license.create') }}' class='btn'>Add License</a>
 @endsection
