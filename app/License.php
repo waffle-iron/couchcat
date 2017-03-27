2e4a24ff6478fb192b3d319e38119e8b100a1cd5
Ryan Eby <eby@ryaneby.com>
2017-03-27 16:58:15 -0400

    break vendor out to it’s own database and setup a relationship

--- app/License.php
+++ app/License.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class License extends Model
 {
-    //
+    public function vendor()
+    {
+        return $this->belongsTo('CouchCat\Vendor');
+    }
 }
--- database/migrations/2017_03_27_161252_create_vendors_table.php
+++ database/migrations/2017_03_27_161252_create_vendors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLicensesTable extends Migration
+class CreateVendorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateLicensesTable extends Migration
      */
     public function up()
     {
-        Schema::create('licenses', function (Blueprint $table) {
+        Schema::create('vendors', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('vendor_id');
-            $table->string('license-stub')->unique();
-            $table->date('starts');
-            $table->date('expires')->nullable();
-            $table->boolean('public')->default(0);
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ class CreateLicensesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('licenses');
+        Schema::dropIfExists('vendors');
     }
 }
